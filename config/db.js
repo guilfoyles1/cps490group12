7abfed4eb07622c725662fe598a4e2fff4a89036
spearsz2 <spearsz2@udayon.edu>
2024-10-28 11:44:21 -0400

    started DB connection & created user DB schema

---
+++ config/db.js
@@ -0,0 +1,23 @@
+//DB info
+const uname = "admin";
+const pword = "CapstoneG12Admin!";
+const cluster = "capstone-1-app"
+const dbname = ""; //defaults to "test" if left blank
+
+//Login to DB
+const uri = `mongodb+srv://${uname}:${pword}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+const mongoose = require('mongoose');
+const mongoose_settings = {useNewUrlParser: true};
+
+//Connect to DB
+mongoose.connect(uri, mongoose_settings);
+const db = mongoose.connection;
+
+//DB connection success/fail
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+    console.log("Connected successfully to MongoDB")
+});
+
+module.exports = {db}
---
+++ models/users.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    uname: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel
